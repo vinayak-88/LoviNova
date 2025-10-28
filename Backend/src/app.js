671e23edd5ee6d5cbd64932da7f8e302038ffb43
Vinayak Goel <vinayak.goel5635@gmail.com>
2025-10-28 11:46:47 +0530

    updated app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -98,7 +98,7 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
   });
 }
